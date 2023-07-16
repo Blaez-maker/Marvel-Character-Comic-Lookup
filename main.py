7a5cc4dc87466ec4f720725a22576dab32e048fa
Blaez-maker <caugustin14@gmail.com>
2023-07-16 14:03:03 -0400

    Updated to use Python 3.10 because of SSL errors with 3.11

--- main.py
+++ main.py
@@ -7,21 +7,20 @@ ts = str(time.time())
 private = "19b1f441208b602d3d4d994c0fda917f83743fd3"
 public = "9aac47d81d48c3c0fb4a6a30c1d48b5b"
 
-hashcode = ts+private+public
-hashcode = hashcode.encode('utf-8')
+hashcode = ts+private+public.encode('utf-8')
 hashcode = hashlib.md5(hashcode).hexdigest()
 
 charname = "Wolverine" #input('Enter the Marvel character:\n')
 
-url = "https://gateway.marvel.com:443/v1/public/characters"
+url = "https://gateway.marvel.com/v1/public/characters"
 
-querystring = {"name":charname,"apikey":public,"hash":hashcode,"ts":ts}
+querystring = {"apikey":public,"hash":hashcode,"ts":ts}
 
 headers = {
     'Marvel-Key': "9aac47d81d48c3c0fb4a6a30c1d48b5b",
     'Marvel-Host': "gateway.marvel.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring, verify=False)
+response = requests.get(url, headers=headers, params=querystring)
 
-print(response.json())
+#print(response.json())
