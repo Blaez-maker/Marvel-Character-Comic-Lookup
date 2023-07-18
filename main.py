ab66f3ecd6edfd0a949d52b0c3590ea9a7f74f91
Blaez-maker <caugustin14@gmail.com>
2023-07-17 23:44:57 -0400

    Updated with match case for different inputs and json conversion

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import requests
 import hashlib
 import time
+import json
 
 ts = str(time.time())
 
 private = "19b1f441208b602d3d4d994c0fda917f83743fd3"
 public = "9aac47d81d48c3c0fb4a6a30c1d48b5b"
 
-hashcode = ts+private+public.encode('utf-8')
+hashcode = (ts+private+public).encode('utf-8')
 hashcode = hashlib.md5(hashcode).hexdigest()
 
-charname = "Wolverine" #input('Enter the Marvel character:\n')
+choice = input('1:Characters\n2:Comic\n3:Creator\n4:Event\n5:Series\n6:Stories\n')
 
-url = "https://gateway.marvel.com/v1/public/characters"
+match choice:
+    case '1':
+        choice = "characters"
+    case '2':
+        choice = "comics"
+    case '3':
+        choice = "creators"
+    case '4':
+        choice = "events"
+    case '5':
+        choice = "series"
+    case '6':
+        choice = "stories"
 
-querystring = {"apikey":public,"hash":hashcode,"ts":ts}
+
+charname = input('Enter the '+ choice + '\n')
+
+url = "https://gateway.marvel.com/v1/public/"+choice
+
+querystring = {"name":charname,"apikey":public,"hash":hashcode,"ts":ts}
 
 headers = {
     'Marvel-Key': "9aac47d81d48c3c0fb4a6a30c1d48b5b",
@@ -23,4 +41,4 @@ headers = {
 
 response = requests.get(url, headers=headers, params=querystring)
 
-#print(response.json())
+print(json.dumps(response.json()))
