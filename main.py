30ebfe4a43523c108c19001fbbc9f1f3782bc316
Blaez-maker <caugustin14@gmail.com>
2023-07-26 23:06:31 -0400

    Updated to pull data from events, series, and stories entries

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import json
 
 ts = str(time.time())
+limit = 600
 
 private = "19b1f441208b602d3d4d994c0fda917f83743fd3"
 public = "9aac47d81d48c3c0fb4a6a30c1d48b5b"
@@ -32,7 +33,7 @@ charname = input('Enter the character\n')
 
 url = "https://gateway.marvel.com/v1/public/characters"
 
-querystring = {"name":charname,"apikey":public,"hash":hashcode,"ts":ts}
+querystring = {"limit": 50, "name": charname, "apikey": public, "hash": hashcode, "ts": ts}
 
 headers = {
     'Marvel-Key': "9aac47d81d48c3c0fb4a6a30c1d48b5b",
@@ -43,10 +44,24 @@ response = requests.get(url, headers=headers, params=querystring)
 
 data = json.loads(json.dumps(response.json()))
 
-jsonResults = data["data"]['results'][0]['comics']
+jsonResults = data['data']['results'][0]['comics']
 
 for x in jsonResults['items']:
      print(x['name'])
 
-#print(data['data']['results'][0]['comics']['items'][1]['name'])
+jsonResults = data['data']['results'][0]['events']
 
+for x in jsonResults['items']:
+     print(x['name'])
+
+jsonResults = data['data']['results'][0]['series']
+
+for x in jsonResults['items']:
+     print(x['name'])
+
+jsonResults = data['data']['results'][0]['stories']
+
+for x in jsonResults['items']:
+     print(x['name'])
+
+#remember to update offset and limit to return the following data
